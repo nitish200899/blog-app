04e5fed9708c9ff6ae23726a66926884877d100c
nitish200899 <nitishkesarwani20@gmail.com>
2024-01-07 22:44:37 +0530

    Added Post div and its styling with its header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">My logo</a>
+        <nav>
+          <a href="" >Login</a>
+          <a href="" >Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="img">
+          <img 
+            src="https://img.freepik.com/free-vector/many-cute-animals-bamboo-forest_1308-32851.jpg?size=626&ext=jpg" 
+            alt="Post image" 
+          />
+        </div>
+        <div className='texts'>
+          <h2>Post title</h2>
+          <p className='info'>
+            <a  className="author">Nitish Kesarwani</a>
+            <time>2024-01-07 22:12</time>
+          </p>
+          <p className='summary'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+            ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="img">
+          <img 
+            src="https://img.freepik.com/free-vector/many-cute-animals-bamboo-forest_1308-32851.jpg?size=626&ext=jpg" 
+            alt="Post image" 
+          />
+        </div>
+        <div className='texts'>
+          <h2>Post title</h2>
+          <p className='info'>
+            <a  className="author">Nitish Kesarwani</a>
+            <time>2024-01-07 22:12</time>
+          </p>
+          <p className='summary'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+            ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="img">
+        <img 
+          src="https://img.freepik.com/free-vector/many-cute-animals-bamboo-forest_1308-32851.jpg?size=626&ext=jpg" 
+          alt="Post image" 
+        />
+        </div>
+        <div className='texts'>
+          <h2>Post title</h2>
+          <p className='info'>
+            <a className="author">Nitish Kesarwani</a>
+            <time>2024-01-07 22:12</time>
+          </p>
+          <p className='summary'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+            commodo consequat.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
