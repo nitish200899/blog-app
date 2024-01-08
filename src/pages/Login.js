0a785a04d2015756fa52b2a075af80d020ec56f9
nitish200899 <nitishkesarwani20@gmail.com>
2024-01-08 23:12:05 +0530

    Login and Register page UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,11 @@
 const Login = () => {
     return (
-        <div>
-           Login
-        </div>
+        <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+        </form>
     )
 }
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,11 @@
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <form className="register">
+            <h1>Register</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Register</button>
+        </form>
     )
 }
 
